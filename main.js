fef65ff1dd4f7d05b9967104466ef20fd242fe4f
RafaHAHAHA <163418292+RafaHAHAHA@users.noreply.github.com>
2024-03-15 10:06:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function mudarimagemfora()
+{
+    document.getElementById("imagem").src="https://static.wikia.nocookie.net/roblox/images/3/35/Manface.png/revision/latest?cb=20211031043454"
+}
+function mudarimagemdentro()
+{
+    document.getElementById("imagem").src="https://ih1.redbubble.net/image.4748549236.6449/flat,750x,075,f-pad,750x1000,f8f8f8.jpg"
+}
